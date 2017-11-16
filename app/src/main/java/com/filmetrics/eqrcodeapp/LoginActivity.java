d714c2b0777bec227b744c4ff6f42c9f47508c82
norbertdmabasa <norbertdmabasa@gmail.com>
2017-11-16 17:59:48 +0800

    Login and Registration

--- app/src/main/java/com/filmetrics/eqrcodeapp/LoginActivity.java
+++ app/src/main/java/com/filmetrics/eqrcodeapp/LoginActivity.java
@@ -1,13 +1,76 @@
 package com.filmetrics.eqrcodeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText usernameTxt;
+    private EditText passwordTxt;
+    private TextView registerHereTxt;
+    private TextView forgotTxt;
+    private TextView guestTxt;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameTxt = (EditText) findViewById(R.id.username_txt_log);
+        passwordTxt = (EditText) findViewById(R.id.password_txt_log);
+
+        registerHereTxt = (TextView) findViewById(R.id.registerhere_txt);
+        forgotTxt = (TextView) findViewById(R.id.forgotpass_txt_log);
+        guestTxt = (TextView) findViewById(R.id.guest_txt_log);
+        registerHereTxt.setOnClickListener(this);
+        forgotTxt.setOnClickListener(this);
+        guestTxt.setOnClickListener(this);
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_btn:
+                onLogin();
+                return;
+            case R.id.registerhere_txt:
+                onRegHere();
+                return;
+            case R.id.forgotpass_txt_log:
+                onForgotPass();
+                return;
+            case R.id.guest_txt_log:
+                onGuest();
+                return;
+            default:
+                return;
+        }
+    }
+
+    private void onLogin() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    private void onRegHere() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    private void onForgotPass() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+
+    private void onGuest() {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/filmetrics/eqrcodeapp/RegistrationActivity.java
+++ app/src/main/java/com/filmetrics/eqrcodeapp/RegistrationActivity.java
@@ -2,12 +2,43 @@ package com.filmetrics.eqrcodeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText usernameEdt;
+    private EditText passwordEdt;
+    private EditText repasswordEdt;
+    private EditText emailAddEdt;
+    private EditText firstnameEdt;
+    private EditText middlenameEdt;
+    private EditText lastnameEdt;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        usernameEdt = (EditText) findViewById(R.id.username_txt_reg);
+        passwordEdt = (EditText) findViewById(R.id.password_txt_reg);
+        repasswordEdt = (EditText) findViewById(R.id.repassword_txt_reg);
+        emailAddEdt = (EditText) findViewById(R.id.email_txt_reg);
+        firstnameEdt = (EditText) findViewById(R.id.firstname_txt_reg);
+        middlenameEdt = (EditText) findViewById(R.id.middlename_txt_reg);
+        lastnameEdt = (EditText) findViewById(R.id.lastname_txt_reg);
+
+        registerBtn = (Button) findViewById(R.id.register_btn);
+        registerBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.register_btn) {
+
+        }
     }
 }
