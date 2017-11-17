2e650996fb36d8bc5ddc3daa967264e71e05edb1
norbertdmabasa <norbertdmabasa@gmail.com>
2017-11-17 14:47:36 +0800

    SplashScreen and QRCode Scanner

---
+++ app/src/main/java/com/filmetrics/eqrcodeapp/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.filmetrics.eqrcodeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
